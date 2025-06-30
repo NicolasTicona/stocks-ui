2a22a6237f7ebaee863d071eed64e73614828bdf
Nicolas Ticona Valdivia <nicolasticonavaldivia@Nicolass-MacBook-Air.local>
2025-06-30 17:27:14 -0500

    fix: add s3 bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
-bucket     = "<YOUR-S3-BACKEND-BUCKET>"
-key        = "<S3-KEY-PATH>/terraform.tfstate"
+bucket     = "stocks-ui-vue-webapp"
+key        = "terraform.tfstate"
 region     = var.region
 access_key = var.access_key
 secret_key = var.secret_key
